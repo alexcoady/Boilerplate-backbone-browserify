b0209cbf8e16d5ce2423518c92e7a777556cfd3e
Alex Coady <alexcoady@stinkdigital.com>
2015-05-28 10:52:04 +0100

    Correct version of BB to suit marionettes dependencies

---
+++ source/templates/templates.js
@@ -0,0 +1,12 @@
+(function() {
+window["JST"] = window["JST"] || {};
+
+window["JST"]["app.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="App-inner">\n  <h2>Well hello there</h2>\n<div>\n';
+
+}
+return __p
+}})();
